96528a4df78918cd0d4c52920c085d6c84c983bb
Mohammad Razak A <mohammadrazaka@Mohammads-Mac-mini.local>
2026-04-29 19:35:56 +0530

    fix: remove acl public-read causing S3 upload errors

--- server/utils/s3.js
+++ server/utils/s3.js
@@ -25,7 +25,6 @@ if (process.env.S3_BUCKET_NAME) {
   storage = multerS3({
     s3: s3,
     bucket: process.env.S3_BUCKET_NAME,
-    acl: 'public-read',
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
